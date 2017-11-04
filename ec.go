875eac94eb14f9f54b398be6011589e6d261e9c1
JP Hastings-Spital <jp@deliveroo.co.uk>
2017-11-04 10:17:53 +0000

    Added support for EC private

--- ec.go
+++ ec.go
@@ -11,11 +11,23 @@ type ECPublic struct {
   Y string `json:"y"`
 }
 
+type ECPrivate struct {
+  *ECPublic
+  D string `json:"d"`
+}
+
 func processECPublic(key *ecdsa.PublicKey) *ECPublic {
   return &ECPublic{
     JWK: &JWK{KeyType: "EC"},
     Curve: key.Params().Name,
     X: asn1b64Encode(key.X),
     Y: asn1b64Encode(key.Y),
+  }
+}
+
+func processECPrivate(key *ecdsa.PrivateKey) *ECPrivate {
+  return &ECPrivate{
+    ECPublic: processECPublic(&key.PublicKey),
+    D: asn1b64Encode(key.D),
   }
 }
--- pem.go
+++ pem.go
@@ -8,14 +8,13 @@ import (
 )
 
 func pemToJwk(pemBytes []byte) string {
-  pemBlock := findPemBlock(pemBytes)
-  keyStruct := processBlock(pemBlock)
+  keyStruct := processBlock(pemBytes)
   return structToJWK(keyStruct)
 }
 
-func findPemBlock(pemBytes []byte) *pem.Block {
+func findPemBlock(pemBytes []byte) (*pem.Block, []byte) {
   // Only get the first PEM block
-  pemBlock, _ := pem.Decode(pemBytes)
+  pemBlock, rest := pem.Decode(pemBytes)
 
   if pemBlock == nil {
     throwParseError("invalid PEM file format.")
@@ -25,19 +24,34 @@ func findPemBlock(pemBytes []byte) *pem.Block {
     throwParseError("the given PEM file is encrypted. Please decrypt first.")
   }
 
-  return pemBlock
+  return pemBlock, rest
 }
 
-func processBlock(pemBlock *pem.Block) interface{} {
+func processBlock(pemBytes []byte) interface{} {
+  pemBlock, rest := findPemBlock(pemBytes)
+
   var keyStruct interface{}
 
-  switch pemBlock.Type  {
+  switch pemBlock.Type {
   case "PUBLIC KEY":
     keyStruct = processPublicKey(pemBlock.Bytes)
   case "RSA PRIVATE KEY":
     key, err := x509.ParsePKCS1PrivateKey(pemBlock.Bytes)
     stopOnParseError(err)
+
     keyStruct = processRSAPrivate(key)
+  case "EC PARAMETERS":
+    // The EC PARAMETERS section appears to not be needed:
+    ecKey, _ := findPemBlock(rest)
+
+    if ecKey.Type != "EC PRIVATE KEY" {
+      throwParseError("unsupported EC PEM format.")
+    }
+
+    key, err := x509.ParseECPrivateKey(ecKey.Bytes)
+    stopOnParseError(err)
+
+    keyStruct = processECPrivate(key)
   default:
     throwParseError("unsupported PEM type.")
   }
