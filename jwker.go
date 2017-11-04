a1a2d295e92d26a787779cf2b75abb69b22c28c4
JP Hastings-Spital <jp@deliveroo.co.uk>
2017-11-04 09:32:59 +0000

    Add usage and ARGV support

--- jwker.go
+++ jwker.go
@@ -6,8 +6,25 @@ import (
   "os"
 )
 
+func usage() {
+  fmt.Print(`jkwer: a PEM <-> JWK conversion tool
+
+Example usage:
+  jwker key.pem > key.jwk
+  jwker key.jwk > key.pem
+
+Create a new keypair, save the public key to a JWK and
+save the private key as a PEM, but with a passphrase:
+
+  openssl ecparam -genkey -name prime256v1 \
+  | tee >(openssl ec -pubout | jwker > key.pub.jwk) \
+  | openssl ec -aes256 -out key.prv.pem
+`)
+  os.Exit(-1)
+}
+
 func main() {
-  bytes, _ := ioutil.ReadAll(os.Stdin)
+  bytes := loadFile()
 
   switch bytes[0] {
   case 45:
@@ -17,6 +34,27 @@ func main() {
   default:
     throwParseError("unknown input file format")
   }
+
+  os.Exit(0)
+}
+
+func loadFile() []byte {
+  file := os.Stdin
+  if len(os.Args) > 1 {
+    namedFile, err := os.Open(os.Args[1])
+    stopOnParseError(err)
+    file = namedFile
+  }
+
+  fileStat, err := file.Stat()
+  stopOnParseError(err)
+  if fileStat.Size() == 0 {
+    usage()
+  }
+
+  bytes, err := ioutil.ReadAll(file)
+  stopOnParseError(err)
+  return bytes
 }
 
 func throwParseError(message string) {
